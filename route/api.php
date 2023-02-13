648315d522103bcff6fde0cba299189cf4f06f91
DREAMLABS <shafs2013@gmail.com>
2023-02-13 22:12:37 +0100

    fix/cors_issue

--- route/api.php
+++ route/api.php
@@ -9,7 +9,7 @@
  header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
  header("Access-Control-Max-Age: 3600");
  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
+ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 
 // Create Router instance
