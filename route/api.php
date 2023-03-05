a9a33b6f34f314775fdaec5b548292ba971093be
acode <acodefix@gmail.com>
2023-03-05 12:40:46 -0800

    cors_issue

--- route/api.php
+++ route/api.php
@@ -4,17 +4,17 @@
  use App\Controllers\userController;
  use App\Traits\Response;
 
-//  header("Access-Control-Allow-Origin: *");
-//  header("Content-Type: application/json; charset=UTF-8");
-//  header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-//  header("Access-Control-Max-Age: 3600");
-//  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- header("Access-Control-Allow-Origin: * ");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE ");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
+ header('Access-Control-Allow-Origin: *');
+ header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+ header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+ header('Content-Type: application/json');
+ $method = $_SERVER['REQUEST_METHOD'];
+ if ($method == "OPTIONS") {
+ header('Access-Control-Allow-Origin: *');
+ header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+ header("HTTP/1.1 200 OK");
+ die();
+ }
 
 
 // Create Router instance
